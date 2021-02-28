20a88cb36566aa1e73e01f4c648a1dd40566199a
Akshh057 <akshh057@gmail.com>
2021-02-28 23:16:52 +0530

    added photo sharing option

---
+++ src/components/TheirMessage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const TheirMessage = ({ lastMessage, message }) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+    return (
+        <div className="message-row">
+            {
+                isFirstMessageByUser && (
+                    <div
+                        className="message-avatar"
+                        style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+                    />
+                )}
+
+            {message?.attachments?.length > 0
+                ? (
+                    <img
+                        src={message.attachments[0].title}
+                        alt="message-attachement"
+                        className="message-image"
+                        style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                    />
+                ) : (
+                    <div className="message" style={{ float: 'left', marginLeft: isFirstMessageByUser ? '4px' : '48px', backgroundColor: '#CABCDC' }}>
+                        {message.text}
+                    </div>
+                )
+
+            }
+        </div>
+    )
+}
+
+export default TheirMessage
